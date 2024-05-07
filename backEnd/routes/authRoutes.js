18a6c0e6147ee69569ff320f6cb41a5a1eeb6e88
Sceyo <aliserfrancis@gmail.com>
2024-05-07 23:50:22 +0800

    added some routes for user and appt

---
+++ backEnd/routes/authRoutes.js
@@ -0,0 +1,26 @@
+
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const database = require("../prisma/database");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+router.post("/register", async (request, response) => {
+  const hashedPassword = await bcrypt.hash(request.body.password, saltRounds);
+
+  const newUser = {
+    name: request.body.name,
+    email: request.body.email,
+    password: hashedPassword,
+    confirmPassword: hashedPassword
+  };
+
+  await database.user.create({
+    data: newUser,
+  });
+
+  response.status(201).json({
+    message: "Account registration successful.",
+  });
+});
