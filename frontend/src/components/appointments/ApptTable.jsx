f62cf3d5734497b2f3dc7b0005f9f14bb93127d3
nichoblina <nichoblina@gmail.com>
2024-05-18 15:24:48 +0800

    feat: implemented data grid to appt page

---
+++ frontend/src/components/appointments/ApptTable.jsx
@@ -0,0 +1,54 @@
+// import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
+export default function AppointmentsTable() {
+  const columns = [
+    { field: 'id', headerName: 'No.', width: 90 },
+    {
+      field: 'reason',
+      headerName: 'Reason for appointment',
+      width: 150,
+    },
+    {
+      field: 'date',
+      headerName: 'Date',
+      type: 'date',
+      width: 150,
+    },
+    {
+      field: 'doctor',
+      headerName: 'Doctor',
+      type: 'number',
+      width: 110,
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 160,
+    },
+  ];
+
+  const rows = [
+    { id: 1, reason: 'Stomach ache', date: '2024-06-17', doctor: 'Dr. Yo Mamma', status: 'Pending' },
+    { id: 2, reason: 'Pregnant because of James Ng', date: '2024-07-30', doctor: 'Dr. James Winston D. Ng', status: 'Pending' },
+  ]
+
+  return (
+    <div>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </div>
+  );
+}
--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -1,18 +1,29 @@
+import { useState } from 'react';
+import AppointmentsTable from '../components/appointments/ApptTable';
 import Header from '../common/Header';
 import Layout from './Layout';
-import { RiAddBoxLine, RiDeleteBinLine } from "react-icons/ri";
-import { FaEdit } from "react-icons/fa";
+import { RiAddBoxLine, RiDeleteBinLine } from 'react-icons/ri';
+import { FaEdit } from 'react-icons/fa';
 
 export default function AppointmentsPage() {
+  const [month, setMonth] = useState('2024-05');
+
   return (
     <Layout>
       <div className='flex flex-1 flex-col bg-slate-50'>
         <Header title='Appointments' searchType={1} />
         <div className='flex flex-col px-16' id='appt-action-bar'>
           <div className='flex flex-1 my-2'>
-            <input type='month' name='appt-month' id='appt-month' />
+            <input
+              type='month'
+              value={month}
+              onChange={(event) => setMonth(event.target.value)}
+              name='appt-month'
+              id='appt-month'
+              className='border-2 border-slate-300 rounded-lg p-2 px-8 text-xl active:border-primary'
+            />
           </div>
-          <div className='flex flex-row'>
+          <div className='flex flex-row my-2'>
             <button className='action-btn flex flex-row items-center'>
               <RiAddBoxLine size={20} style={{ marginRight: '8px' }} />
               Set Appointment
@@ -27,6 +38,7 @@ export default function AppointmentsPage() {
             </button>
           </div>
         </div>
+        <AppointmentsTable />
       </div>
     </Layout>
   );
